f65e173da6344731b3238bf98ac0485bef4b5d1e
David Goodrich <djgoodrich15@gmail.com>
2017-09-13 14:08:37 -0400

    update files

--- models/blog.js
+++ models/blog.js
@@ -47,3 +47,4 @@ module.exports = function(sequelize, DataTypes) {
     return Blog;
   };
   
+  
--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,6 +1,11 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function(app, auth) {
  
-
+    app.get("/login", auth.requiresLogin, function(req, res) {
+        console.log(req.user)
+        .then(function(results) {
+            res.redirect("/");
+        });
+    });
 };
