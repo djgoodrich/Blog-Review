c9a54189a62dd95f6fa8db589ff4423535679388
David Goodrich <djgoodrich15@gmail.com>
2017-09-15 14:31:25 -0400

    added write review

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,84 +5,109 @@ var db = require("../models");
 var path = require("path");
 
 module.exports = function(app, auth) {
-    //auth.requiresLogin if want to add login
-    // Send to homepage with top ten blogs displayed
-    app.get("/", function(req, res) {
-        var userSub;
-        if(req.user) {
-            userSub = req.user._json.sub
-        };
-        console.log(userSub)
-        db.Blog.findAll( {
-           limit: 12,
-           order: [['cumulative_rating', 'DESC']] 
-        }).then(function(results) {
-            res.render("index", {
-                topTen: results,
-                currentUserSub: userSub
-            });
-        })
-    });
-
-    // Send to blog page with title, website, description, cumulative rating displayed at top; reviews listed below.
-    app.get("/blog/:blog_title", function(req, res) {
-        var userSub;
-        if(req.user) {
-            userSub = req.user._json.sub
-        };
-        // Get specific blog
-        db.Blog.findOne({
-           include: {
-               model : db.Review,
-               include : [db.User]
-           },
-           where: {
-               id: req.params.blog_title
-           }
-       }).then(function(dbBlog){
-           console.log(JSON.stringify(dbBlog))
-           res.render("blog", {
-               blog : dbBlog,
-               currentUserSub: userSub
-            });
-       })
-   });
-
-    // Send to user page with list of reviews/ratings; if you are the user whose page you are on, you can edit any of your reviews from here.
-    app.get("/user/:id", function(req, res) {
-        var userSub;
-        if(req.user) {
-            userSub = req.user._json.sub
-        };
-        // Get users sorted by number of reviews
-        db.User.findOne({
-            include: {
-                model: db.Review,
-                include: [db.Blog]
-            },
-            where: {
-                id: req.params.id
-            }
-        }).then(function(dbUser){
-            console.log(JSON.stringify(dbUser))
-            res.render("user", {
-               user: dbUser,
-               currentUserSub: userSub
-            });
-        })
-
-    });
-    app.post("/searchBlogs", function(req, res) {
-        req.body.keyword = req.body.keyword.trim();
-        db.Blog.findAll({
-            where: {
-                title: {
-                    like: "%" + req.body.keyword + "%"
-                }
-            }
-        }).then(function(dbBlog){
-            console.log(JSON.stringify(dbBlog))
-            res.render("searchResults", {blogs : dbBlog});
+  //auth.requiresLogin if want to add login
+  // Send to homepage with top ten blogs displayed
+  app.get("/", function(req, res) {
+    var userSub;
+    if (req.user) {
+      userSub = req.user._json.sub;
+    }
+    console.log(userSub);
+    db.Blog
+      .findAll({
+        limit: 12,
+        order: [["cumulative_rating", "DESC"]]
+      })
+      .then(function(results) {
+        res.render("index", {
+          topTen: results,
+          currentUserSub: userSub
         });
-    });
+      });
+  });
+
+  // Send to blog page with title, website, description, cumulative rating displayed at top; reviews listed below.
+  app.get("/blog/:blog_title", function(req, res) {
+    var userSub;
+    if (req.user) {
+      userSub = req.user._json.sub;
+    }
+    // Get specific blog
+    db.Blog
+      .findOne({
+        include: {
+          model: db.Review,
+          include: [db.User]
+        },
+        where: {
+          id: req.params.blog_title
+        }
+      })
+      .then(function(dbBlog) {
+        console.log(JSON.stringify(dbBlog));
+        res.render("blog", {
+          blog: dbBlog,
+          currentUserSub: userSub
+        });
+      });
+  });
+
+  // Send to user page with list of reviews/ratings; if you are the user whose page you are on, you can edit any of your reviews from here.
+  app.get("/user/:id", function(req, res) {
+    var userSub;
+    if (req.user) {
+      userSub = req.user._json.sub;
+    }
+    // Get users sorted by number of reviews
+    db.User
+      .findOne({
+        include: {
+          model: db.Review,
+          include: [db.Blog]
+        },
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbUser) {
+        console.log(JSON.stringify(dbUser));
+        res.render("user", {
+          user: dbUser,
+          currentUserSub: userSub
+        });
+      });
+  });
+  app.post("/searchBlogs", function(req, res) {
+    var userSub;
+    if (req.user) {
+      userSub = req.user._json.sub;
+    }
+    req.body.keyword = req.body.keyword.trim();
+    db.Blog
+      .findAll({
+        where: {
+          title: {
+            like: "%" + req.body.keyword + "%"
+          }
+        }
+      })
+      .then(function(dbBlog) {
+        console.log(JSON.stringify(dbBlog));
+
+        if (typeof dbBlog[0] === "undefined") {
+          res.render("searchResults", {
+            blogs: dbBlog,
+            foundResults: false,
+            currentUserSub: userSub
+          });
+        } else {
+            console.log("working else")
+          res.render("searchResults", {
+            blogs: dbBlog,
+            foundResults: true,
+            currentUserSub: userSub
+          });
+        }
+      });
+  });
 };
