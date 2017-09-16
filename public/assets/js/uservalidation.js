c15b54079bd518e69514c227b303db514128bd36
iamtheluckyest <annalisa.alden@gmail.com>
2017-09-16 12:08:28 -0400

    writeReviewModal only shows if review by that author for that blog does not already exist

--- public/assets/js/uservalidation.js
+++ public/assets/js/uservalidation.js
@@ -22,4 +22,3 @@ function isEmpty(field, event) {
         return event.preventDefault();
     };
 };
-
--- routes/review-api-routes.js
+++ routes/review-api-routes.js
@@ -1,87 +1,90 @@
 var db = require("../models");
 
 module.exports = function(app) {
-    app.get("/api/review/by/:userId", function(req, res) {
-
-        db.Review.findOne({
-            where: {
-                UserId : req.params.userId
-            }
-        }).then(function(dbReview) {
-            res.json(dbReview);
-        });
-    });
-
-    app.get("/api/reviews/:id", function(req, res) {
-        db.Review.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [db.Blog]
-        }).then(function(dbReview) {
-            res.json(dbReview);
-        });
-    });
+    // app.get("/api/reviews/:id", function(req, res) {
+    //     db.Review.findOne({
+    //         where: {
+    //             id: req.params.id
+    //         },
+    //         include: [db.Blog]
+    //     }).then(function(dbReview) {
+    //         res.json(dbReview);
+    //     });
+    // });
 
     app.post("/api/reviews", function(req, res) {
         // Gets the user id using the user sub from auth0
         db.User.findOne({
             where: {
                 sub : req.user._json.sub
-            }, 
-            include : [db.Review]
-        }).then(function(dbUser){
-            // Need to find the index of the array element that has id = to the blog currently being reviewed, if exists.
-            var result = dbUser.Reviews.filter(function(obj) {
-                console.log(obj.BlogId);
-                return obj.BlogId == req.body.BlogId;
-            });
-            if(typeof result[0] === "undefined") {
-                // Adds user id as FK to the Review to associate it with the user submitting the review
-                req.body.UserId = dbUser.id;
-                req.body.rating = parseFloat(req.body.rating);
-                try {
-                    db.Review.create(req.body).then(function(dbReview) {
-                        // Gets the blog's current total review count and cumulative rating.
-                        db.Blog.findOne({
-                            where: {
-                                id : req.body.BlogId
-                            }
-                        }).then(function(dbBlog){
-                            var newReviewCount = 1;
-                            var newRating = req.body.rating;                   
-                            if (dbBlog.total_reviews){
-                                newReviewCount += dbBlog.total_reviews;
-                                newRating = (dbBlog.cumulative_rating * dbBlog.total_reviews + newRating) / (newReviewCount);
-                            };
-                            // Updates the blog's total review count and cumulative rating.
-                            db.Blog.update(
-                                {
-                                    total_reviews: newReviewCount,
-                                    cumulative_rating: newRating
-                                },
-                                {
-                                where : {
-                                    id : req.body.BlogId
-                                }
-                            }).then(function(dbUpdatedBlog){
-                                res.redirect("/blog/" + req.body.BlogId);
-                            })
-                        })
-                    });
-                } catch (err) {
-                    res.json({
-                        message: err.message
-                    })                    
-                }
-            } else {
-                // Inform user that review has already been submitted; redirect to edit review.
-                console.log("A review has already been submitted")
             }
+        }).then(function(dbUser){
+            // Adds user id as FK to the Review to associate it with the user submitting the review
+            req.body.UserId = dbUser.id;
+            req.body.rating = parseFloat(req.body.rating);
+            db.Review.create(req.body).then(function(dbReview) {
+                // Gets the blog's current total review count and cumulative rating.
+                db.Blog.findOne({
+                    where: {
+                        id : req.body.BlogId
+                    }
+                }).then(function(dbBlog){
+                    var newReviewCount = 1;
+                    var newRating = req.body.rating;                   
+                    if (dbBlog.total_reviews){
+                        newReviewCount += dbBlog.total_reviews;
+                        newRating = (dbBlog.cumulative_rating * dbBlog.total_reviews + newRating) / (newReviewCount);
+                    };
+                    // Updates the blog's total review count and cumulative rating.
+                    db.Blog.update(
+                        {
+                            total_reviews: newReviewCount,
+                            cumulative_rating: newRating
+                        },
+                        {
+                        where : {
+                            id : req.body.BlogId
+                        }
+                    }).then(function(dbUpdatedBlog){
+                        res.redirect("/blog/" + req.body.BlogId);
+                    })
+                })
+            });
         })
     });
 
-    app.delete("/api/reviews/:id", function(req, res) {
+    app.get("/api/review/:blogId/:sub", function(req, res) {
+        // Finds the user
+        db.User.findOne({
+            where: {
+                sub: req.params.sub
+            }
+        }).then(function(dbUser){
+            // Finds any reviews that having already been written for by that user, for that blog
+            db.Review.findOne({
+                where: {
+                    UserId : dbUser.id,
+                    BlogId : req.params.blogId
+                }
+            }).then(function(dbReview) {
+                console.log("Review by this author for this blog already exists.")
+                res.json(dbReview);
+            });
+        });
+    });
+
+    app.get("api/review/edit/:id", function(req, res){
+        db.Review.findOne({
+            where: {
+                id : req.params.id
+            }
+        }).then(function(dbReview){
+            // Send info back to modal
+        })
+    });
+
+
+    app.delete("/api/review/:id", function(req, res) {
         db.Review.destroy({
             where: {
                 id: req.params.id
@@ -91,16 +94,17 @@ module.exports = function(app) {
         });
     });
 
-    app.put("/api/reviews", function(req, res) {
-        db.Review.update(
-            req.body,
-            {
-                where: {
-                    id: req.body.id
-                }
+    app.put("/api/reviews/:id", function(req, res) {
+        db.Review.update({
+            rating: req.body.rating,
+            title: req.body.title,
+            body: req.body.body
+        },{
+            where: {
+                id: req.params.id
             }
-        ).then(function(dbReview) {
-            res.json(dbReview);
+        }).then(function(dbUpdatedReview) {
+            res.json(dbUpdatedReview);
         });
     });
 }
